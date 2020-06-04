66dd688bff7474fba1c073a175847ecf2fe2c1c7
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-06-03 23:46:42 -0500

    Updated logging

--- Example/JavaScriptKitExample/Sources/JavaScriptKitExample/main.swift
+++ Example/JavaScriptKitExample/Sources/JavaScriptKitExample/main.swift
@@ -5,19 +5,27 @@ let document = JSObjectRef.global.document.object!
 let bluetooth = JSBluetooth.shared!
 
 let divElement = document.createElement!("div").object!
-divElement.innerText = "Hello, world"
+divElement.innerText = "Swift Bluetooth Web App"
 let body = document.body.object!
 _ = body.appendChild!(divElement)
 
 let buttonElement = document.createElement!("button").object!
-buttonElement.innerText = "Click me!"
+buttonElement.innerText = "Scan for Bluetooth devices"
 buttonElement.onclick = .function { _ in
+    JSConsole.info("Swift is running on browser!")
     JSConsole.debug("\(#file) \(#function) \(#line)")
     alert("Swift is running on browser!")
-    JSConsole.log("Requesting device")
+    JSConsole.log("Requesting any Bluetooth Device...")
     bluetooth.requestDevice().then {
-        JSConsole.info("\($0)")
+        JSConsole.info($0)
+        JSConsole.debug("\(#file) \(#function) \(#line)")
         alert("Got device \($0)")
+        JSConsole.log("Connecting to GATT Server...")
+        $0.gatt.connect()
+    }.then {
+        JSConsole.info($0)
+        JSConsole.debug("\(#file) \(#function) \(#line)")
+        alert("Connected")
     }
     JSConsole.debug("\(#file) \(#function) \(#line)")
     return .undefined
--- Sources/JavaScriptKit/JS Types/JSConsole.swift
+++ Sources/JavaScriptKit/JS Types/JSConsole.swift
@@ -22,36 +22,36 @@ public final class JSConsole: JSType {
     /**
      The Console method `log()` outputs a message to the web console. The message may be a single string (with optional substitution values), or it may be any one or more JavaScript objects.
      */
-    public static func log(_ message: String) {
-        logFunction(message)
+    public static func log(_ arguments: JSValueConvertible...) {
+        logFunction.dynamicallyCall(withArguments: arguments)
     }
     
     /**
      The `console.info()` method outputs an informational message to the Web Console. In Firefox, a small "i" icon is displayed next to these items in the Web Console's log.
      */
-    public static func info(_ message: String) {
-        infoFunction(message)
+    public static func info(_ arguments: JSValueConvertible...) {
+        infoFunction.dynamicallyCall(withArguments: arguments)
     }
     
     /**
      The console method `debug()` outputs a message to the web console at the "debug" log level. The message is only displayed to the user if the console is configured to display debug output.
      */
-    public static func debug(_ message: String) {
-        debugFunction(message)
+    public static func debug(_ arguments: JSValueConvertible...) {
+        debugFunction.dynamicallyCall(withArguments: arguments)
     }
     
     /**
      Outputs an error message to the Web Console.
      */
-    public static func error(_ message: String) {
-        errorFunction(message)
+    public static func error(_ arguments: JSValueConvertible...) {
+        errorFunction.dynamicallyCall(withArguments: arguments)
     }
     
     /**
      The `console.assert()` method writes an error message to the console if the assertion is false. If the assertion is true, nothing happens.
      */
-    public static func assert(_ condition: @autoclosure () -> (Bool), _ message: String) {
-        assertFunction(condition(), message)
+    public static func assert(_ condition: @autoclosure () -> (Bool), _ arguments: JSValueConvertible...) {
+        assertFunction.dynamicallyCall(withArguments: [condition()] + arguments)
     }
 }
 
@@ -69,3 +69,16 @@ internal extension JSConsole {
     
     static let assertFunction = classObject.assert.function!
 }
+/*
+private extension JSConsole {
+    
+    /// Console should print objects as their description and not
+    static func print(_ value: JSValueConvertible) -> JSValueConvertible {
+        if let value = value as? CustomStringConvertible {
+            return value.description
+        } else {
+            return value
+        }
+    }
+}
+*/
