7eb458790a83f6f0a5b0ac5edec0e196bc48d74d
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-06-04 02:06:40 -0500

    Updated `JSPromise`

--- Sources/JavaScriptKit/JS Types/JSPromise.swift
+++ Sources/JavaScriptKit/JS Types/JSPromise.swift
@@ -10,7 +10,7 @@
  
  A Promise is a proxy for a value not necessarily known when the promise is created. It allows you to associate handlers with an asynchronous action's eventual success value or failure reason. This lets asynchronous methods return values like synchronous methods: instead of immediately returning the final value, the asynchronous method returns a promise to supply the value at some point in the future.
  */
-public final class JSPromise<Success: JSType>: JSType {
+public final class JSPromise<Success>: JSType where Success: JSValueConvertible, Success: JSValueConstructible {
     
     // MARK: - Properties
     
