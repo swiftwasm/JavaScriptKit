2f0eae8941b42db060cac6611b0c1cc427f6cc87
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-06-03 20:09:29 -0500

    Updated example

--- Example/JavaScriptKitExample/Sources/JavaScriptKitExample/main.swift
+++ Example/JavaScriptKitExample/Sources/JavaScriptKitExample/main.swift
@@ -2,6 +2,7 @@ import JavaScriptKit
 
 let alert = JSObjectRef.global.alert.function!
 let document = JSObjectRef.global.document.object!
+let bluetooth = JSBluetooth.shared!
 
 let divElement = document.createElement!("div").object!
 divElement.innerText = "Hello, world"
@@ -11,7 +12,14 @@ _ = body.appendChild!(divElement)
 let buttonElement = document.createElement!("button").object!
 buttonElement.innerText = "Click me!"
 buttonElement.onclick = .function { _ in
+    JSObjectRef.global.console.object?.log.function?("\(#file) \(#function) \(#line)")
     alert("Swift is running on browser!")
+    bluetooth.requestDevice().then {
+        JSObjectRef.global.console.object?.log.function?("\($0)")
+        alert("Got device \($0)")
+    }
+    JSObjectRef.global.console.object?.log.function?("\(#file) \(#function) \(#line)")
+    return .undefined
 }
 
 _ = body.appendChild!(buttonElement)
