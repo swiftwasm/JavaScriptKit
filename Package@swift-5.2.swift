45e4c5597454288864b40fadb97bdb8eba93b4bb
Max Desiatov <max@desiatov.com>
2020-06-16 15:45:52 +0100

    Add Package@swift-5.2.swift to support the older version

---
+++ Package@swift-5.2.swift
@@ -0,0 +1,40 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "JavaScriptKit",
+    products: [
+        .library(name: "JavaScriptKit", targets: ["JavaScriptKit"])
+    ],
+    targets: [
+        .target(
+            name: "JavaScriptKit",
+            dependencies: ["_CJavaScriptKit"],
+            linkerSettings: [
+                .unsafeFlags(
+                    [
+                        "-Xlinker",
+                        "--export=swjs_call_host_function",
+                        "-Xlinker",
+                        "--export=swjs_prepare_host_function_call",
+                        "-Xlinker",
+                        "--export=swjs_cleanup_host_function_call"
+                    ]
+                )
+            ]),
+        .target(
+            name: "_CJavaScriptKit",
+            linkerSettings: [
+                .unsafeFlags(
+                    [
+                        "-Xlinker",
+                        "--allow-undefined",
+                    ]
+                )
+            ]),
+        .testTarget(
+            name: "JavaScriptKitTests",
+            dependencies: ["JavaScriptKit"]),
+    ]
+)
