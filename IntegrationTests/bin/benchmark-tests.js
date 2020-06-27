df0c35961fd0249b0db252309000480359b0c063
Yuta Saito <kateinoigakukun@gmail.com>
2020-06-27 15:56:56 +0900

    Add JS benchmarks

--- IntegrationTests/bin/benchmark-tests.js
+++ IntegrationTests/bin/benchmark-tests.js
@@ -9,6 +9,34 @@ global.benchmarkRunner = function(name, body) {
   console.log("done " + (endTime - startTime) + " ms");
 }
 
+class JSBenchmark {
+    constructor(title) { this.title = title }
+    testSuite(name, body) {
+        benchmarkRunner(`${this.title}/${name}`, (iteration) => {
+            for (let idx = 0; idx < iteration; idx++) {
+                body()
+            }
+        })
+    }
+}
+
+const serialization = new JSBenchmark("Serialization")
+serialization.testSuite("Write JavaScript number directly", () => {
+    const jsNumber = 42
+    const object = global
+    for (let idx = 0; idx < 100; idx++) {
+        object["numberValue" + idx] = jsNumber
+    }
+})
+
+serialization.testSuite("Write JavaScript string directly", () => {
+    const jsString = "Hello, world"
+    const object = global
+    for (let idx = 0; idx < 100; idx++) {
+        object["stringValue" + idx] = jsString
+    }
+})
+
 startWasiTask("./dist/BenchmarkTests.wasm").catch(err => {
   console.log(err)
 });
