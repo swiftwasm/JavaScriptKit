74a464af9e07a839bef11f114d44b82a2f31aae1
Max Desiatov <max@desiatov.com>
2020-09-15 09:48:17 +0100

    Add doc comments

--- Sources/JavaScriptKit/BasicObjects/JSTimer.swift
+++ Sources/JavaScriptKit/BasicObjects/JSTimer.swift
@@ -3,12 +3,20 @@ pairs of calls for you, and also holds a reference to an underlying `JSClosure` 
 To invalidate a timer you should deallocate your corresponding `JSTimer` instance.
 */
 public final class JSTimer {
+  /// Indicates whether this timer instance calls its callback repeatedly at a given delay.
+  public let isRepeating: Bool
+
   private let closure: JSClosure
-  private let isRepeating: Bool
+
+  /** Node.js and browser APIs are slightly different. `setTimeout`/`setInterval` return an object
+  in Node.js, while browsers return a number. Fortunately, clearTimeout and clearInterval take
+  corresponding types as their arguments, and we can store either as JSValue, so we can treat both
+  cases uniformly.
+  */
   private let value: JSValue
   private let global = JSObject.global
 
-  public init(millisecondsDelay: Double, isRepeating: Bool, callback: @escaping () -> ()) {
+  public init(millisecondsDelay: Double, isRepeating: Bool = false, callback: @escaping () -> ()) {
     closure = JSClosure { _ in callback() }
     self.isRepeating = isRepeating
     if isRepeating {
@@ -18,6 +26,10 @@ public final class JSTimer {
     }
   }
 
+  /** Makes a corresponding `clearTimeout` or `clearInterval` call, depending on whether this timer
+  instance is repeating. The `closure` instance is released manually here, as it is required for
+  bridged closure instances.
+  */
   deinit {
     if isRepeating {
       global.clearInterval.function!(value)
