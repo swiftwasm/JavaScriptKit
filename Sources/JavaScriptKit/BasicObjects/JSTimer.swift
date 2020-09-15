e3258e5452ef1ff5d3326de851490006757f1c97
Max Desiatov <max@desiatov.com>
2020-09-15 12:18:56 +0100

    Improve `JSTimer` doc comments

--- Sources/JavaScriptKit/BasicObjects/JSTimer.swift
+++ Sources/JavaScriptKit/BasicObjects/JSTimer.swift
@@ -1,4 +1,7 @@
-/** This timer type hides `setInterval`/`clearInterval` and `setTimeout`/`clearTimeout`
+/** This timer type hides [`setInterval`](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval)
+/ [`clearInterval`](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/clearInterval) and 
+[`setTimeout`](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout)
+/ [`clearTimeout`](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout)
 pairs of calls for you, and also holds a reference to an underlying `JSClosure` instance.
 To invalidate a timer you should deallocate your corresponding `JSTimer` instance.
 */
@@ -16,6 +19,15 @@ public final class JSTimer {
   private let value: JSValue
   private let global = JSObject.global
 
+  /**
+  Creates a new timer instance that calls `setInterval` or `setTimeout` JavaScript functions for you
+  under the hood.
+  - Parameters:
+    - millisecondsDelay: the amount of milliseconds before the `callback` closure is executed.
+    - isRepeating: when `true` the `callback` closure is executed repeatedly at given 
+    `millisecondsDelay` intervals indefinitely until the timer is deallocated.
+    - callback: the closure to be executed after a given `millisecondsDelay` interval.
+  */
   public init(millisecondsDelay: Double, isRepeating: Bool = false, callback: @escaping () -> ()) {
     closure = JSClosure { _ in callback() }
     self.isRepeating = isRepeating
