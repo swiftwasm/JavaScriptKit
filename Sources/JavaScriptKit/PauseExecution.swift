bcfa3cb919811430347eba4d3a52be2c8a5b3fe3
Jonathan Hemi <jonathan@hemi.dev>
2021-01-30 22:39:32 +0800

    Use new simplified JSPromise API

--- Sources/JavaScriptKit/PauseExecution.swift
+++ Sources/JavaScriptKit/PauseExecution.swift
@@ -9,7 +9,7 @@ public func pauseExecution(milliseconds: Int32) {
 }
 
 
-extension JSPromise where Success == JSValue, Failure == JSError {
+extension JSPromise {
     /// Unwind Wasm module execution stack and rewind it after promise resolves,
     /// allowing JavaScript events to continue to be processed in the meantime.
     /// - Parameters:
@@ -18,7 +18,7 @@ extension JSPromise where Success == JSValue, Failure == JSError {
     ///
     /// **Important**: Wasm module must be [asyncified](https://emscripten.org/docs/porting/asyncify.html),
     /// otherwise JavaScriptKit's runtime will throw an exception.
-    public func syncAwait() -> Result<Success, Failure> {
+    public func syncAwait() -> Result<JSValue, JSValue> {
         var kindAndFlags = JavaScriptValueKindAndFlags()
         var payload1 = JavaScriptPayload1()
         var payload2 = JavaScriptPayload2()
@@ -26,11 +26,7 @@ extension JSPromise where Success == JSValue, Failure == JSError {
         _syncAwait(jsObject.id, &kindAndFlags, &payload1, &payload2)
         let result = RawJSValue(kind: kindAndFlags.kind, payload1: payload1, payload2: payload2).jsValue()
         if kindAndFlags.isException {
-            if let error = JSError(from: result) {
-                return .failure(error)
-            } else {
-                return .failure(JSError(message: "Could not build proper JSError from result \(result)"))
-            }
+            return .failure(result)
         } else {
             return .success(result)
         }
