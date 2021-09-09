728443580c81afc40e6b89ef4c52350854f3dacc
Yuta Saito <kateinoigakukun@gmail.com>
2021-09-09 16:40:40 -0700

    Bump Runtime version

--- Runtime/src/index.ts
+++ Runtime/src/index.ts
@@ -141,7 +141,7 @@ export class SwiftRuntime {
     private instance: WebAssembly.Instance | null;
     private heap: SwiftRuntimeHeap;
     private _closureHeap: SwiftClosureHeap | null;
-    private version: number = 701;
+    private version: number = 702;
 
     constructor() {
         this.instance = null;
