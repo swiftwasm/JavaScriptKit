5c38015a663d67c2d18204790dde3368999ba5bb
Jed Fox <git@jedfox.com>
2022-01-08 10:55:49 -0500

    convert from method to property

--- Runtime/src/index.ts
+++ Runtime/src/index.ts
@@ -101,259 +101,258 @@ export class SwiftRuntime {
         return output;
     }
 
-    importObjects() {
-        return {
-            swjs_set_prop: (
-                ref: ref,
-                name: ref,
-                kind: JavaScriptValueKind,
-                payload1: number,
-                payload2: number
-            ) => {
-                const obj = this.memory.getObject(ref);
-                Reflect.set(
+    /** @deprecated Use `wasmImports` instead */
+    importObjects = () => this.wasmImports;
+
+    readonly wasmImports = {
+        swjs_set_prop: (
+            ref: ref,
+            name: ref,
+            kind: JavaScriptValueKind,
+            payload1: number,
+            payload2: number
+        ) => {
+            const obj = this.memory.getObject(ref);
+            Reflect.set(
+                obj,
+                this.memory.getObject(name),
+                decodeJSValue(kind, payload1, payload2, this.memory)
+            );
+        },
+
+        swjs_get_prop: (
+            ref: ref,
+            name: ref,
+            kind_ptr: pointer,
+            payload1_ptr: pointer,
+            payload2_ptr: pointer
+        ) => {
+            const obj = this.memory.getObject(ref);
+            const result = Reflect.get(obj, this.memory.getObject(name));
+            writeJSValue(
+                result,
+                kind_ptr,
+                payload1_ptr,
+                payload2_ptr,
+                false,
+                this.memory
+            );
+        },
+
+        swjs_set_subscript: (
+            ref: ref,
+            index: number,
+            kind: JavaScriptValueKind,
+            payload1: number,
+            payload2: number
+        ) => {
+            const obj = this.memory.getObject(ref);
+            Reflect.set(
+                obj,
+                index,
+                decodeJSValue(kind, payload1, payload2, this.memory)
+            );
+        },
+
+        swjs_get_subscript: (
+            ref: ref,
+            index: number,
+            kind_ptr: pointer,
+            payload1_ptr: pointer,
+            payload2_ptr: pointer
+        ) => {
+            const obj = this.memory.getObject(ref);
+            const result = Reflect.get(obj, index);
+            writeJSValue(
+                result,
+                kind_ptr,
+                payload1_ptr,
+                payload2_ptr,
+                false,
+                this.memory
+            );
+        },
+
+        swjs_encode_string: (ref: ref, bytes_ptr_result: pointer) => {
+            const bytes = this.textEncoder.encode(this.memory.getObject(ref));
+            const bytes_ptr = this.memory.retain(bytes);
+            this.memory.writeUint32(bytes_ptr_result, bytes_ptr);
+            return bytes.length;
+        },
+
+        swjs_decode_string: (bytes_ptr: pointer, length: number) => {
+            const bytes = this.memory.bytes.subarray(
+                bytes_ptr,
+                bytes_ptr + length
+            );
+            const string = this.textDecoder.decode(bytes);
+            return this.memory.retain(string);
+        },
+
+        swjs_load_string: (ref: ref, buffer: pointer) => {
+            const bytes = this.memory.getObject(ref);
+            this.memory.writeBytes(buffer, bytes);
+        },
+
+        _swjs_call_function: (
+            ref: ref,
+            argv: pointer,
+            argc: number,
+            kind_ptr: pointer,
+            payload1_ptr: pointer,
+            payload2_ptr: pointer
+        ) => {
+            const func = this.memory.getObject(ref);
+            let result: any;
+            try {
+                result = Reflect.apply(
+                    func,
+                    undefined,
+                    decodeJSValueArray(argv, argc, this.memory)
+                );
+            } catch (error) {
+                writeJSValue(
+                    error,
+                    kind_ptr,
+                    payload1_ptr,
+                    payload2_ptr,
+                    true,
+                    this.memory
+                );
+                return;
+            }
+            writeJSValue(
+                result,
+                kind_ptr,
+                payload1_ptr,
+                payload2_ptr,
+                false,
+                this.memory
+            );
+        },
+        get swjs_call_function() {
+            return this._swjs_call_function;
+        },
+        set swjs_call_function(value) {
+            this._swjs_call_function = value;
+        },
+
+        swjs_call_function_with_this: (
+            obj_ref: ref,
+            func_ref: ref,
+            argv: pointer,
+            argc: number,
+            kind_ptr: pointer,
+            payload1_ptr: pointer,
+            payload2_ptr: pointer
+        ) => {
+            const obj = this.memory.getObject(obj_ref);
+            const func = this.memory.getObject(func_ref);
+            let result: any;
+            try {
+                result = Reflect.apply(
+                    func,
                     obj,
-                    this.memory.getObject(name),
-                    decodeJSValue(kind, payload1, payload2, this.memory)
+                    decodeJSValueArray(argv, argc, this.memory)
                 );
-            },
-
-            swjs_get_prop: (
-                ref: ref,
-                name: ref,
-                kind_ptr: pointer,
-                payload1_ptr: pointer,
-                payload2_ptr: pointer
-            ) => {
-                const obj = this.memory.getObject(ref);
-                const result = Reflect.get(obj, this.memory.getObject(name));
+            } catch (error) {
                 writeJSValue(
-                    result,
+                    error,
                     kind_ptr,
                     payload1_ptr,
                     payload2_ptr,
-                    false,
+                    true,
                     this.memory
                 );
-            },
+                return;
+            }
+            writeJSValue(
+                result,
+                kind_ptr,
+                payload1_ptr,
+                payload2_ptr,
+                false,
+                this.memory
+            );
+        },
+        swjs_call_new: (ref: ref, argv: pointer, argc: number) => {
+            const constructor = this.memory.getObject(ref);
+            const instance = Reflect.construct(
+                constructor,
+                decodeJSValueArray(argv, argc, this.memory)
+            );
+            return this.memory.retain(instance);
+        },
 
-            swjs_set_subscript: (
-                ref: ref,
-                index: number,
-                kind: JavaScriptValueKind,
-                payload1: number,
-                payload2: number
-            ) => {
-                const obj = this.memory.getObject(ref);
-                Reflect.set(
-                    obj,
-                    index,
-                    decodeJSValue(kind, payload1, payload2, this.memory)
-                );
-            },
-
-            swjs_get_subscript: (
-                ref: ref,
-                index: number,
-                kind_ptr: pointer,
-                payload1_ptr: pointer,
-                payload2_ptr: pointer
-            ) => {
-                const obj = this.memory.getObject(ref);
-                const result = Reflect.get(obj, index);
-                writeJSValue(
-                    result,
-                    kind_ptr,
-                    payload1_ptr,
-                    payload2_ptr,
-                    false,
-                    this.memory
-                );
-            },
-
-            swjs_encode_string: (ref: ref, bytes_ptr_result: pointer) => {
-                const bytes = this.textEncoder.encode(
-                    this.memory.getObject(ref)
-                );
-                const bytes_ptr = this.memory.retain(bytes);
-                this.memory.writeUint32(bytes_ptr_result, bytes_ptr);
-                return bytes.length;
-            },
-
-            swjs_decode_string: (bytes_ptr: pointer, length: number) => {
-                const bytes = this.memory.bytes.subarray(
-                    bytes_ptr,
-                    bytes_ptr + length
-                );
-                const string = this.textDecoder.decode(bytes);
-                return this.memory.retain(string);
-            },
-
-            swjs_load_string: (ref: ref, buffer: pointer) => {
-                const bytes = this.memory.getObject(ref);
-                this.memory.writeBytes(buffer, bytes);
-            },
-
-            _swjs_call_function: (
-                ref: ref,
-                argv: pointer,
-                argc: number,
-                kind_ptr: pointer,
-                payload1_ptr: pointer,
-                payload2_ptr: pointer
-            ) => {
-                const func = this.memory.getObject(ref);
-                let result: any;
-                try {
-                    result = Reflect.apply(
-                        func,
-                        undefined,
-                        decodeJSValueArray(argv, argc, this.memory)
-                    );
-                } catch (error) {
-                    writeJSValue(
-                        error,
-                        kind_ptr,
-                        payload1_ptr,
-                        payload2_ptr,
-                        true,
-                        this.memory
-                    );
-                    return;
-                }
-                writeJSValue(
-                    result,
-                    kind_ptr,
-                    payload1_ptr,
-                    payload2_ptr,
-                    false,
-                    this.memory
-                );
-            },
-            get swjs_call_function() {
-                return this._swjs_call_function;
-            },
-            set swjs_call_function(value) {
-                this._swjs_call_function = value;
-            },
-
-            swjs_call_function_with_this: (
-                obj_ref: ref,
-                func_ref: ref,
-                argv: pointer,
-                argc: number,
-                kind_ptr: pointer,
-                payload1_ptr: pointer,
-                payload2_ptr: pointer
-            ) => {
-                const obj = this.memory.getObject(obj_ref);
-                const func = this.memory.getObject(func_ref);
-                let result: any;
-                try {
-                    result = Reflect.apply(
-                        func,
-                        obj,
-                        decodeJSValueArray(argv, argc, this.memory)
-                    );
-                } catch (error) {
-                    writeJSValue(
-                        error,
-                        kind_ptr,
-                        payload1_ptr,
-                        payload2_ptr,
-                        true,
-                        this.memory
-                    );
-                    return;
-                }
-                writeJSValue(
-                    result,
-                    kind_ptr,
-                    payload1_ptr,
-                    payload2_ptr,
-                    false,
-                    this.memory
-                );
-            },
-            swjs_call_new: (ref: ref, argv: pointer, argc: number) => {
-                const constructor = this.memory.getObject(ref);
-                const instance = Reflect.construct(
+        swjs_call_throwing_new: (
+            ref: ref,
+            argv: pointer,
+            argc: number,
+            exception_kind_ptr: pointer,
+            exception_payload1_ptr: pointer,
+            exception_payload2_ptr: pointer
+        ) => {
+            const constructor = this.memory.getObject(ref);
+            let result: any;
+            try {
+                result = Reflect.construct(
                     constructor,
                     decodeJSValueArray(argv, argc, this.memory)
                 );
-                return this.memory.retain(instance);
-            },
-
-            swjs_call_throwing_new: (
-                ref: ref,
-                argv: pointer,
-                argc: number,
-                exception_kind_ptr: pointer,
-                exception_payload1_ptr: pointer,
-                exception_payload2_ptr: pointer
-            ) => {
-                const constructor = this.memory.getObject(ref);
-                let result: any;
-                try {
-                    result = Reflect.construct(
-                        constructor,
-                        decodeJSValueArray(argv, argc, this.memory)
-                    );
-                } catch (error) {
-                    writeJSValue(
-                        error,
-                        exception_kind_ptr,
-                        exception_payload1_ptr,
-                        exception_payload2_ptr,
-                        true,
-                        this.memory
-                    );
-                    return -1;
-                }
+            } catch (error) {
                 writeJSValue(
-                    null,
+                    error,
                     exception_kind_ptr,
                     exception_payload1_ptr,
                     exception_payload2_ptr,
-                    false,
+                    true,
                     this.memory
                 );
-                return this.memory.retain(result);
-            },
+                return -1;
+            }
+            writeJSValue(
+                null,
+                exception_kind_ptr,
+                exception_payload1_ptr,
+                exception_payload2_ptr,
+                false,
+                this.memory
+            );
+            return this.memory.retain(result);
+        },
 
-            swjs_instanceof: (obj_ref: ref, constructor_ref: ref) => {
-                const obj = this.memory.getObject(obj_ref);
-                const constructor = this.memory.getObject(constructor_ref);
-                return obj instanceof constructor;
-            },
+        swjs_instanceof: (obj_ref: ref, constructor_ref: ref) => {
+            const obj = this.memory.getObject(obj_ref);
+            const constructor = this.memory.getObject(constructor_ref);
+            return obj instanceof constructor;
+        },
 
-            swjs_create_function: (host_func_id: number) => {
-                const func = (...args: any[]) =>
-                    this.callHostFunction(host_func_id, args);
-                const func_ref = this.memory.retain(func);
-                this.closureDeallocator?.track(func, func_ref);
-                return func_ref;
-            },
+        swjs_create_function: (host_func_id: number) => {
+            const func = (...args: any[]) =>
+                this.callHostFunction(host_func_id, args);
+            const func_ref = this.memory.retain(func);
+            this.closureDeallocator?.track(func, func_ref);
+            return func_ref;
+        },
 
-            swjs_create_typed_array: (
-                constructor_ref: ref,
-                elementsPtr: pointer,
-                length: number
-            ) => {
-                const ArrayType: TypedArray =
-                    this.memory.getObject(constructor_ref);
-                const array = new ArrayType(
-                    this.memory.rawMemory.buffer,
-                    elementsPtr,
-                    length
-                );
-                // Call `.slice()` to copy the memory
-                return this.memory.retain(array.slice());
-            },
+        swjs_create_typed_array: (
+            constructor_ref: ref,
+            elementsPtr: pointer,
+            length: number
+        ) => {
+            const ArrayType: TypedArray =
+                this.memory.getObject(constructor_ref);
+            const array = new ArrayType(
+                this.memory.rawMemory.buffer,
+                elementsPtr,
+                length
+            );
+            // Call `.slice()` to copy the memory
+            return this.memory.retain(array.slice());
+        },
 
-            swjs_release: (ref: ref) => {
-                this.memory.release(ref);
-            },
-        };
-    }
+        swjs_release: (ref: ref) => {
+            this.memory.release(ref);
+        },
+    };
 }
