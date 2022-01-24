6e7e5d8c4ceba3fd9c05e5b874d5f7c736a41b3a
yonihemi <jonathan@hemi.dev>
2022-01-24 16:22:06 +0800

    Increment runtime version

--- Runtime/src/index.ts
+++ Runtime/src/index.ts
@@ -141,7 +141,7 @@ export class SwiftRuntime {
     private instance: WebAssembly.Instance | null;
     private heap: SwiftRuntimeHeap;
     private _closureHeap: SwiftClosureHeap | null;
-    private version: number = 703;
+    private version: number = 704;
 
     constructor() {
         this.instance = null;
