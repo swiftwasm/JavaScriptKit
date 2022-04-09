8c02c79f3da19f6c3c3d79ef01473899ecc5e593
Jed Fox <git@jedfox.com>
2022-04-09 10:10:06 -0400

    Convert BigInts into Swift ints automatically

--- Sources/JavaScriptKit/ConstructibleFromJSValue.swift
+++ Sources/JavaScriptKit/ConstructibleFromJSValue.swift
@@ -20,80 +20,44 @@ extension String: ConstructibleFromJSValue {
     }
 }
 
-extension Double: ConstructibleFromJSValue {
-    public static func construct(from value: JSValue) -> Double? {
-        return value.number
-    }
-}
-
-extension Float: ConstructibleFromJSValue {
-    public static func construct(from value: JSValue) -> Float? {
-        return value.number.map(Float.init)
-    }
-}
-
-extension Int: ConstructibleFromJSValue {
-    public static func construct(from value: JSValue) -> Self? {
-        value.number.map(Self.init)
-    }
-}
-
-extension Int8: ConstructibleFromJSValue {
-    public static func construct(from value: JSValue) -> Self? {
-        value.number.map(Self.init)
-    }
-}
-
-extension Int16: ConstructibleFromJSValue {
-    public static func construct(from value: JSValue) -> Self? {
-        value.number.map(Self.init)
-    }
-}
-
-extension Int32: ConstructibleFromJSValue {
-    public static func construct(from value: JSValue) -> Self? {
-        value.number.map(Self.init)
-    }
-}
-
-extension Int64: ConstructibleFromJSValue {
-    public static func construct(from value: JSValue) -> Self? {
-        value.number.map(Self.init)
-    }
-}
-
-extension UInt: ConstructibleFromJSValue {
-    public static func construct(from value: JSValue) -> Self? {
-        value.number.map(Self.init)
-    }
-}
-
-extension UInt8: ConstructibleFromJSValue {
-    public static func construct(from value: JSValue) -> Self? {
-        value.number.map(Self.init)
-    }
-}
-
-extension UInt16: ConstructibleFromJSValue {
-    public static func construct(from value: JSValue) -> Self? {
-        value.number.map(Self.init)
-    }
-}
-
-extension UInt32: ConstructibleFromJSValue {
-    public static func construct(from value: JSValue) -> Self? {
-        value.number.map(Self.init)
-    }
-}
-
-extension UInt64: ConstructibleFromJSValue {
-    public static func construct(from value: JSValue) -> Self? {
-        value.number.map(Self.init)
-    }
-}
-
 extension JSString: ConstructibleFromJSValue {
     public static func construct(from value: JSValue) -> JSString? {
         value.jsString
     }
 }
+
+extension BinaryFloatingPoint where Self: ConstructibleFromJSValue {
+    public static func construct(from value: JSValue) -> Self? {
+        value.number.map(Self.init)
+    }
+}
+extension Double: ConstructibleFromJSValue {}
+extension Float: ConstructibleFromJSValue {}
+
+extension SignedInteger where Self: ConstructibleFromJSValue {
+    public init(_ bigInt: JSBigInt) {
+        self.init(bigInt.int64Value)
+    }
+    public static func construct(from value: JSValue) -> Self? {
+        value.bigInt.map(Self.init) ?? value.number.map(Self.init)
+    }
+}
+extension Int: ConstructibleFromJSValue {}
+extension Int8: ConstructibleFromJSValue {}
+extension Int16: ConstructibleFromJSValue {}
+extension Int32: ConstructibleFromJSValue {}
+extension Int64: ConstructibleFromJSValue {}
+
+extension UnsignedInteger where Self: ConstructibleFromJSValue {
+    public init(_ bigInt: JSBigInt) {
+        self.init(bigInt.uInt64Value)
+    }
+    public static func construct(from value: JSValue) -> Self? {
+        value.bigInt.map(Self.init) ?? value.number.map(Self.init)
+    }
+}
+extension UInt: ConstructibleFromJSValue {}
+extension UInt8: ConstructibleFromJSValue {}
+extension UInt16: ConstructibleFromJSValue {}
+extension UInt32: ConstructibleFromJSValue {}
+extension UInt64: ConstructibleFromJSValue {}
