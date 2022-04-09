bf3bac3e1d25560cad02ec828a1ab15b4e61ec0d
Jed Fox <git@jedfox.com>
2022-04-09 10:01:36 -0400

    Add JSBigInt/clamped method, fix overload

--- Sources/JavaScriptKit/FundamentalObjects/JSBigInt.swift
+++ Sources/JavaScriptKit/FundamentalObjects/JSBigInt.swift
@@ -1,15 +1,19 @@
 import _CJavaScriptKit
 
-public class JSBigInt: JSObject {
-    var int64Value: Int64 { _bigint_to_i64(id, true) }
-    var uInt64Value: UInt64 {
+private let constructor = JSObject.global.BigInt.function!
+
+public final class JSBigInt: JSObject {
+    public var int64Value: Int64 {
+        _bigint_to_i64(id, true)
+    }
+    public var uInt64Value: UInt64 {
         UInt64(bitPattern: _bigint_to_i64(id, false))
     }
 
     public convenience init(_ value: Int64) {
         self.init(id: _i64_to_bigint(value, true))
     }
-    public convenience init(_ value: UInt64) {
+    public convenience init(unsigned value: UInt64) {
         self.init(id: _i64_to_bigint(Int64(bitPattern: value), false))
     }
 
@@ -23,5 +27,13 @@ public class JSBigInt: JSObject {
 
     override public var jsValue: JSValue {
         .bigInt(self)
+    }
+
+    public func clamped(bitSize: Int, signed: Bool) -> JSBigInt {
+        if signed {
+            return constructor.asIntN!(bitSize, self).bigInt!
+        } else {
+            return constructor.asUintN!(bitSize, self).bigInt!
+        }
     }
 }
