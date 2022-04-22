5cff1425369819c7b61e234e93b2cd441af96848
Jed Fox <git@jedfox.com>
2022-04-22 09:52:15 -0400

    fix typo
    Co-Authored-By: Max Desiatov <max@desiatov.com>

--- Sources/JavaScriptKit/XcodeSupport.swift
+++ Sources/JavaScriptKit/XcodeSupport.swift
@@ -1,7 +1,7 @@
 import _CJavaScriptKit
 
 /// Note:
-/// Define all runtime functions stub which are imported from JavaScript environment.
+/// Define all runtime function stubs which are imported from JavaScript environment.
 /// SwiftPM doesn't support WebAssembly target yet, so we need to define them to
 /// avoid link failure.
 /// When running with JavaScript runtime library, they are ignored completely.
