69cd8c94552c3ce0f043beefde3dbc99b5c9c4d2
Yuta Saito <kateinoigakukun@gmail.com>
2024-05-08 12:26:54 +0000

    Remove Wasmer WASI tests
    We no longer use it in carton

--- IntegrationTests/lib.js
+++ IntegrationTests/lib.js
@@ -1,6 +1,4 @@
 const SwiftRuntime = require("javascript-kit-swift").SwiftRuntime;
-const WasmerWASI = require("@wasmer/wasi").WASI;
-const WasmFs = require("@wasmer/wasmfs").WasmFs;
 const NodeWASI = require("wasi").WASI;
 const { WASI: MicroWASI, useAll } = require("uwasi");
 
@@ -9,41 +7,6 @@ const fs = require("fs");
 const readFile = promisify(fs.readFile);
 
 const WASI = {
-    Wasmer: ({ programName }) => {
-        // Instantiate a new WASI Instance
-        const wasmFs = new WasmFs();
-        // Output stdout and stderr to console
-        const originalWriteSync = wasmFs.fs.writeSync;
-        wasmFs.fs.writeSync = (fd, buffer, offset, length, position) => {
-            const text = new TextDecoder("utf-8").decode(buffer);
-            switch (fd) {
-                case 1:
-                    console.log(text);
-                    break;
-                case 2:
-                    console.error(text);
-                    break;
-            }
-            return originalWriteSync(fd, buffer, offset, length, position);
-        };
-        const wasi = new WasmerWASI({
-            args: [programName],
-            env: {},
-            bindings: {
-                ...WasmerWASI.defaultBindings,
-                fs: wasmFs.fs,
-            },
-        });
-
-        return {
-            wasiImport: wasi.wasiImport,
-            start(instance) {
-                wasi.start(instance);
-                instance.exports._initialize();
-                instance.exports.main();
-            }
-        }
-    },
     MicroWASI: ({ programName }) => {
         const wasi = new MicroWASI({
             args: [programName],
