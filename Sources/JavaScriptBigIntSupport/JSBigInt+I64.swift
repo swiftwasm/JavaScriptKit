aef882c6dfbc45f14e2e99686ab689b764e58b58
Yuta Saito <kateinoigakukun@gmail.com>
2024-06-11 20:02:24 +0900

    Migrate JavaScriptBigIntSupport module

--- Sources/JavaScriptBigIntSupport/JSBigInt+I64.swift
+++ Sources/JavaScriptBigIntSupport/JSBigInt+I64.swift
@@ -3,18 +3,18 @@ import _CJavaScriptBigIntSupport
 
 extension JSBigInt: JSBigIntExtended {
     public var int64Value: Int64 {
-        _bigint_to_i64(id, true)
+        swjs_bigint_to_i64(id, true)
     }
 
     public var uInt64Value: UInt64 {
-        UInt64(bitPattern: _bigint_to_i64(id, false))
+        UInt64(bitPattern: swjs_bigint_to_i64(id, false))
     }
 
     public convenience init(_ value: Int64) {
-        self.init(id: _i64_to_bigint(value, true))
+        self.init(id: swjs_i64_to_bigint(value, true))
     }
 
     public convenience init(unsigned value: UInt64) {
-        self.init(id: _i64_to_bigint(Int64(bitPattern: value), false))
+        self.init(id: swjs_i64_to_bigint(Int64(bitPattern: value), false))
     }
 }
--- Sources/_CJavaScriptBigIntSupport/include/_CJavaScriptKit+I64.h
+++ Sources/_CJavaScriptBigIntSupport/include/_CJavaScriptKit+I64.h
@@ -4,24 +4,26 @@
 
 #include <_CJavaScriptKit.h>
 
+#if __wasm32__
+# define IMPORT_JS_FUNCTION(name, returns, args) \
+__attribute__((__import_module__("javascript_kit"), __import_name__(#name))) extern returns name args;
+#else
+# define IMPORT_JS_FUNCTION(name, returns, args) \
+  static inline returns name args { \
+    abort(); \
+  }
+#endif
+
 /// Converts the provided Int64 or UInt64 to a BigInt.
 ///
 /// @param value The value to convert.
 /// @param is_signed Whether to treat the value as a signed integer or not.
-#if __wasi__
-__attribute__((__import_module__("javascript_kit"),
-               __import_name__("swjs_i64_to_bigint")))
-#endif
-extern JavaScriptObjectRef _i64_to_bigint(const long long value, bool is_signed);
+IMPORT_JS_FUNCTION(swjs_i64_to_bigint, JavaScriptObjectRef, (const long long value, bool is_signed))
 
 /// Converts the provided BigInt to an Int64 or UInt64.
 ///
 /// @param ref The target JavaScript object.
 /// @param is_signed Whether to treat the return value as a signed integer or not.
-#if __wasi__
-__attribute__((__import_module__("javascript_kit"),
-               __import_name__("swjs_bigint_to_i64")))
-#endif
-extern long long _bigint_to_i64(const JavaScriptObjectRef ref, bool is_signed);
+IMPORT_JS_FUNCTION(swjs_bigint_to_i64, long long, (const JavaScriptObjectRef ref, bool is_signed))
 
 #endif /* _CJavaScriptBigIntSupport_h */
