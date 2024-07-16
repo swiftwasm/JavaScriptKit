a3a0a47bdc7e04d3fa10c515169e2cf2595ea1d6
Yuta Saito <kateinoigakukun@gmail.com>
2024-07-16 11:45:59 +0900

    Update multithreading example to be compatible with older Xcode

--- Examples/Multithreading/Package.swift
+++ Examples/Multithreading/Package.swift
@@ -1,9 +1,10 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 
 import PackageDescription
 
 let package = Package(
     name: "Example",
+    platforms: [.macOS("15"), .iOS("18"), .watchOS("11"), .tvOS("18"), .visionOS("2")],
     dependencies: [
         .package(path: "../../"),
         .package(url: "https://github.com/kateinoigakukun/chibi-ray", revision: "c8cab621a3338dd2f8e817d3785362409d3b8cf1"),
