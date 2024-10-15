a4636bbc5537216d59246b7c6de07edb6b0752c0
Yuta Saito <kateinoigakukun@gmail.com>
2024-10-15 11:12:07 +0900

    Simplify the embedded example
    * Remove EmbeddedFoundation dependency
    * Prefix the experimental feature flags with `JAVASCRIPTKIT_`
    * Remove the unnecessary C shim for `memmove`, which is now provided by `swift-dlmalloc`
    * Add the `arc4random_buf` shim for the latest Hashable support

--- Examples/Embedded/Package.swift
+++ Examples/Embedded/Package.swift
@@ -6,14 +6,31 @@ let package = Package(
     name: "Embedded",
     dependencies: [
         .package(name: "JavaScriptKit", path: "../../"),
-        .package(url: "https://github.com/swifweb/EmbeddedFoundation", branch: "0.1.0")
+        .package(url: "https://github.com/swiftwasm/swift-dlmalloc", branch: "0.1.0")
     ],
     targets: [
         .executableTarget(
             name: "EmbeddedApp",
             dependencies: [
                 "JavaScriptKit",
-                .product(name: "Foundation", package: "EmbeddedFoundation")
+                .product(name: "dlmalloc", package: "swift-dlmalloc")
+            ],
+            cSettings: [
+                .unsafeFlags(["-fdeclspec"])
+            ],
+            swiftSettings: [
+                .enableExperimentalFeature("Embedded"),
+                .enableExperimentalFeature("Extern"),
+                .unsafeFlags([
+                    "-Xfrontend", "-gnone",
+                    "-Xfrontend", "-disable-stack-protector",
+                ]),
+            ],
+            linkerSettings: [
+                .unsafeFlags([
+                    "-Xclang-linker", "-nostdlib",
+                    "-Xlinker", "--no-entry"
+                ])
             ]
         )
     ]
--- Examples/Embedded/build.sh
+++ Examples/Embedded/build.sh
@@ -1,11 +1,7 @@
-EXPERIMENTAL_EMBEDDED_WASM=true swift build -c release --product EmbeddedApp \
+JAVASCRIPTKIT_EXPERIMENTAL_EMBEDDED_WASM=true swift build -c release --product EmbeddedApp \
   --triple wasm32-unknown-none-wasm  \
   -Xswiftc -enable-experimental-feature -Xswiftc Embedded \
   -Xswiftc -enable-experimental-feature -Xswiftc Extern \
-  -Xswiftc -wmo -Xswiftc -disable-cmo \
-  -Xswiftc -Xfrontend -Xswiftc -gnone \
-  -Xswiftc -Xfrontend -Xswiftc -disable-stack-protector \
-  -Xswiftc -cxx-interoperability-mode=default \
   -Xcc -D__Embedded -Xcc -fdeclspec \
   -Xlinker --export-if-defined=__main_argc_argv \
   -Xlinker --export-if-defined=swjs_call_host_function \
