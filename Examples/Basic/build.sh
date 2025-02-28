39ae1865653d05ace9d3685811141f812c433b52
Yuta Saito <kateinoigakukun@gmail.com>
2025-02-28 09:01:15 +0000

    CI: Use Swift SDK by default
    In other words, drop toolchain-style installation support

--- Examples/Basic/build.sh
+++ Examples/Basic/build.sh
@@ -1,1 +1,2 @@
-swift build --swift-sdk DEVELOPMENT-SNAPSHOT-2024-09-20-a-wasm32-unknown-wasi -Xswiftc -Xclang-linker -Xswiftc -mexec-model=reactor -Xlinker --export=__main_argc_argv
+#!/bin/bash
+swift build --swift-sdk "${SWIFT_SDK_ID:-wasm32-unknown-wasi}" -Xswiftc -Xclang-linker -Xswiftc -mexec-model=reactor -Xlinker --export=__main_argc_argv
