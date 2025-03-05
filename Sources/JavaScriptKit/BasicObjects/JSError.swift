daa820960939fceef1aa243af9a1ac84dc724712
Yuta Saito <kateinoigakukun@gmail.com>
2025-03-05 06:29:04 +0000

    Concurrency: Remove `Error` conformance from `JSError`
    `Error` protocol now requires `Sendable` conformance, which is not
    possible for `JSError` because `JSObject` is not `Sendable`.

--- Sources/JavaScriptKit/BasicObjects/JSError.swift
+++ Sources/JavaScriptKit/BasicObjects/JSError.swift
@@ -2,17 +2,13 @@
  class](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error) that
  exposes its properties in a type-safe way.
  */
-public final class JSError: Error, JSBridgedClass {
+public final class JSError: JSBridgedClass {
     /// The constructor function used to create new JavaScript `Error` objects.
     public static var constructor: JSFunction? { _constructor.wrappedValue }
     private static let _constructor = LazyThreadLocal(initialize: { JSObject.global.Error.function })
 
     /// The underlying JavaScript `Error` object.
-    ///
-    /// NOTE: This property must be accessed from the thread that
-    ///       the thrown `Error` object was created on. Otherwise,
-    ///       it will result in a runtime assertion failure.
-    public nonisolated(unsafe) let jsObject: JSObject
+    public let jsObject: JSObject
 
     /// Creates a new instance of the JavaScript `Error` class with a given message.
     public init(message: String) {
