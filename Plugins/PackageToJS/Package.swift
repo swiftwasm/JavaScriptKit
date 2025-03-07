1b2b2f5a53d2482149ffaeb7260a0382b9e6e4f8
Yuta Saito <kateinoigakukun@gmail.com>
2025-03-07 11:44:55 +0900

    Fix build on macOS

--- Plugins/PackageToJS/Package.swift
+++ Plugins/PackageToJS/Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "PackageToJS",
+    platforms: [.macOS(.v13)],
     targets: [
         .target(name: "PackageToJS"),
         .testTarget(name: "PackageToJSTests", dependencies: ["PackageToJS"]),
