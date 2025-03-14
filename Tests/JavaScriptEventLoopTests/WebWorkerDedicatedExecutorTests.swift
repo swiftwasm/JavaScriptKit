9c38b8f2f1b3184cae69d8f81e315d6f18ef1782
Yuta Saito <kateinoigakukun@gmail.com>
2025-03-14 11:51:31 +0000

    Add test cases for `WebWorkerDedicatedExecutor`

---
+++ Tests/JavaScriptEventLoopTests/WebWorkerDedicatedExecutorTests.swift
@@ -0,0 +1,32 @@
+import XCTest
+@testable import JavaScriptEventLoop
+
+final class WebWorkerDedicatedExecutorTests: XCTestCase {
+    actor MyActor {
+        let executor: WebWorkerDedicatedExecutor
+        nonisolated var unownedExecutor: UnownedSerialExecutor {
+            self.executor.asUnownedSerialExecutor()
+        }
+
+        init(executor: WebWorkerDedicatedExecutor) {
+            self.executor = executor
+            XCTAssertTrue(isMainThread())
+        }
+
+        func onWorkerThread() async {
+            XCTAssertFalse(isMainThread())
+            await Task.detached {}.value
+            // Should keep on the thread after back from the other isolation domain
+            XCTAssertFalse(isMainThread())
+        }
+    }
+
+    func testEnqueue() async throws {
+        let executor = try await WebWorkerDedicatedExecutor()
+        defer { executor.terminate() }
+        let actor = MyActor(executor: executor)
+        XCTAssertTrue(isMainThread())
+        await actor.onWorkerThread()
+        XCTAssertTrue(isMainThread())
+    }
+}
