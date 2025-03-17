ec9470c122bf7ffed0834a13e5c848340858ec36
Yuta Saito <kateinoigakukun@gmail.com>
2025-03-17 07:21:42 +0000

    PackageToJS: Fetch module from the default location if `init` is called without options

--- Plugins/PackageToJS/Templates/index.js
+++ Plugins/PackageToJS/Templates/index.js
@@ -3,12 +3,16 @@ import { instantiate } from './instantiate.js';
 import { defaultBrowserSetup /* #if USE_SHARED_MEMORY */, createDefaultWorkerFactory /* #endif */} from './platforms/browser.js';
 
 /** @type {import('./index.d').init} */
-export async function init(moduleSource) {
-    const options = await defaultBrowserSetup({
-        module: moduleSource,
+export async function init(options = {}) {
+    let module = options.module;
+    if (!module) {
+        module = fetch(new URL("@PACKAGE_TO_JS_MODULE_PATH@", import.meta.url))
+    }
+    const instantiateOptions = await defaultBrowserSetup({
+        module,
 /* #if USE_SHARED_MEMORY */
         spawnWorker: createDefaultWorkerFactory()
 /* #endif */
     })
-    return await instantiate(options);
+    return await instantiate(instantiateOptions);
 }
