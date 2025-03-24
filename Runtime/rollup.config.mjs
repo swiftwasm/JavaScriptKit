7751f9c776f46cd48f82e662f0161e946e6c9e54
Yuta Saito <kateinoigakukun@gmail.com>
2025-03-24 07:45:22 +0000

    Bundle d.ts files with rollup-plugin-dts

--- Runtime/rollup.config.mjs
+++ Runtime/rollup.config.mjs
@@ -1,20 +1,31 @@
 import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 
 /** @type {import('rollup').RollupOptions} */
-const config = {
-    input: "src/index.ts",
-    output: [
-        {
-            file: "lib/index.mjs",
+const config = [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: "lib/index.mjs",
+                format: "esm",
+            },
+            {
+                file: "lib/index.js",
+                format: "umd",
+                name: "JavaScriptKit",
+            },
+        ],
+        plugins: [typescript()],
+    },
+    {
+        input: "src/index.ts",
+        output: {
+            file: "lib/index.d.ts",
             format: "esm",
         },
-        {
-            dir: "lib",
-            format: "umd",
-            name: "JavaScriptKit",
-        },
-    ],
-    plugins: [typescript()],
-};
+        plugins: [dts()],
+    },
+];
 
 export default config;
