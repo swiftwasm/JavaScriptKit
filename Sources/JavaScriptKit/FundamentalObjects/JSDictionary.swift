5c7e75e526fe6cf971d3653bb38e3de8494651e2
Yuta Saito <kateinoigakukun@gmail.com>
2025-03-25 15:13:37 +0000

    ./Utilities/format.swift

--- Sources/JavaScriptKit/FundamentalObjects/JSDictionary.swift
+++ Sources/JavaScriptKit/FundamentalObjects/JSDictionary.swift
@@ -1,34 +1,34 @@
 import _CJavaScriptKit
 
 public final class JSDictionary {
-  private let ref: JSObject
+    private let ref: JSObject
 
-  init(ref: JSObject) { self.ref = ref }
+    init(ref: JSObject) { self.ref = ref }
 
-  public init() { ref = JSObject(id: swjs_create_object()) }
+    public init() { ref = JSObject(id: swjs_create_object()) }
 
-  public subscript(key: String) -> JSValue {
-    get { ref[dynamicMember: key] }
-    set { ref[dynamicMember: key] = newValue }
-  }
+    public subscript(key: String) -> JSValue {
+        get { ref[dynamicMember: key] }
+        set { ref[dynamicMember: key] = newValue }
+    }
 }
 
 extension JSDictionary: ExpressibleByDictionaryLiteral {
-  public convenience init(dictionaryLiteral elements: (String, JSValue)...) {
-    self.init()
+    public convenience init(dictionaryLiteral elements: (String, JSValue)...) {
+        self.init()
 
-    for (key, value) in elements { self[key] = value }
-  }
+        for (key, value) in elements { self[key] = value }
+    }
 }
 
 extension JSDictionary: ConvertibleToJSValue {
-  public var jsValue: JSValue { .object(ref) }
+    public var jsValue: JSValue { .object(ref) }
 }
 
 extension JSDictionary: ConstructibleFromJSValue {
-  public static func construct(from value: JSValue) -> JSDictionary? {
-    guard let object = value.object else { return nil }
+    public static func construct(from value: JSValue) -> JSDictionary? {
+        guard let object = value.object else { return nil }
 
-    return JSDictionary(ref: object)
-  }
+        return JSDictionary(ref: object)
+    }
 }
