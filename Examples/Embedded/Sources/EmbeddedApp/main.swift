f41f2340279641428ef3a3072867489015926660
Yuta Saito <kateinoigakukun@gmail.com>
2025-03-27 09:24:30 +0000

    Add an example of using TypedArray in an embedded app

--- Examples/Embedded/Sources/EmbeddedApp/main.swift
+++ Examples/Embedded/Sources/EmbeddedApp/main.swift
@@ -11,18 +11,49 @@ var divElement = document.createElement("div")
 divElement.innerText = .string("Count \(count)")
 _ = document.body.appendChild(divElement)
 
-var buttonElement = document.createElement("button")
-buttonElement.innerText = "Click me"
-buttonElement.onclick = JSValue.object(
+var clickMeElement = document.createElement("button")
+clickMeElement.innerText = "Click me"
+clickMeElement.onclick = JSValue.object(
     JSClosure { _ in
         count += 1
         divElement.innerText = .string("Count \(count)")
         return .undefined
     }
 )
+_ = document.body.appendChild(clickMeElement)
 
-_ = document.body.appendChild(buttonElement)
+var encodeResultElement = document.createElement("pre")
+var textInputElement = document.createElement("input")
+textInputElement.type = "text"
+textInputElement.placeholder = "Enter text to encode to UTF-8"
+textInputElement.oninput = JSValue.object(
+    JSClosure { _ in
+        let textEncoder = JSObject.global.TextEncoder.function!.new()
+        let encode = textEncoder.encode.function!
+        let encodedData = JSTypedArray<UInt8>(
+            unsafelyWrapping: encode(this: textEncoder, textInputElement.value).object!
+        )
+        encodeResultElement.innerText = .string(
+            encodedData.withUnsafeBytes { bytes in
+                bytes.map { hex($0) }.joined(separator: " ")
+            }
+        )
+        return .undefined
+    }
+)
+let encoderContainer = document.createElement("div")
+_ = encoderContainer.appendChild(textInputElement)
+_ = encoderContainer.appendChild(encodeResultElement)
+_ = document.body.appendChild(encoderContainer)
 
 func print(_ message: String) {
     _ = JSObject.global.console.log(message)
+}
+
+func hex(_ value: UInt8) -> String {
+    var result = "0x"
+    let hexChars: [Character] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+    result.append(hexChars[Int(value / 16)])
+    result.append(hexChars[Int(value % 16)])
+    return result
 }
