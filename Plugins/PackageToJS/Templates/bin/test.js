67c9782f8394afde200d7044e50ccad900fb72fd
Yuta Saito <kateinoigakukun@gmail.com>
2025-05-07 09:31:17 +0800

    PackageToJS: Report stack trace on `proc_exit`

--- Plugins/PackageToJS/Templates/bin/test.js
+++ Plugins/PackageToJS/Templates/bin/test.js
@@ -42,7 +42,12 @@ const harnesses = {
             let options = await nodePlatform.defaultNodeSetup({
                 args: testFrameworkArgs,
                 onExit: (code) => {
-                    if (code !== 0) { return }
+                    if (code !== 0) {
+                        const stack = new Error().stack
+                        console.error(`Test failed with exit code ${code}`)
+                        console.error(stack)
+                        return
+                    }
                     // Extract the coverage file from the wasm module
                     const filePath = "default.profraw"
                     const destinationPath = args.values["coverage-file"] ?? filePath
