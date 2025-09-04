e0e1740c330c05b542c9779f869e03b03a5daccb
Yuta Saito <kateinoigakukun@gmail.com>
2025-09-04 20:13:17 +0900

    Update WASI target from wasm32-unknown-wasi to wasm32-unknown-wasip1

--- Examples/PlayBridgeJS/build.sh
+++ Examples/PlayBridgeJS/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
-env JAVASCRIPTKIT_EXPERIMENTAL_BRIDGEJS=1 swift package --swift-sdk "${SWIFT_SDK_ID_wasm32_unknown_wasi:-${SWIFT_SDK_ID:-wasm32-unknown-wasi}}" -c "${1:-debug}" \
+env JAVASCRIPTKIT_EXPERIMENTAL_BRIDGEJS=1 swift package --swift-sdk "${SWIFT_SDK_ID_wasm32_unknown_wasip1:-${SWIFT_SDK_ID:-wasm32-unknown-wasip1}}" -c "${1:-debug}" \
     plugin --allow-writing-to-package-directory \
     js --use-cdn --output ./Bundle
