b9fe8aa3221d5bc7674325db304268f77044856e
Yuta Saito <kateinoigakukun@gmail.com>
2025-09-22 00:44:56 +0000

    WIP

--- Examples/Embedded/Package.swift
+++ Examples/Embedded/Package.swift
@@ -4,16 +4,11 @@ import PackageDescription
 
 let package = Package(
     name: "Embedded",
-    dependencies: [
-        .package(name: "JavaScriptKit", path: "../../"),
-        .package(url: "https://github.com/swiftwasm/swift-dlmalloc", branch: "0.1.0"),
-    ],
     targets: [
         .executableTarget(
             name: "EmbeddedApp",
             dependencies: [
                 "MinJSK",
-                .product(name: "dlmalloc", package: "swift-dlmalloc"),
             ],
             cSettings: [.unsafeFlags(["-fdeclspec"])],
             swiftSettings: [
