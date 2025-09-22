666ac24a2b47aea8e2c7f49b510becdc6ae11286
Yuta Saito <kateinoigakukun@gmail.com>
2025-09-22 01:09:14 +0000

    WIP

--- Examples/Embedded/repro.sh
+++ Examples/Embedded/repro.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-JAVASCRIPTKIT_EXPERIMENTAL_EMBEDDED_WASM=true swift build --package-path /home/katei/ghq/github.com/swiftwasm/JavaScriptKit/Examples/Embedded -c release --triple wasm32-unknown-none-wasm
+JAVASCRIPTKIT_EXPERIMENTAL_EMBEDDED_WASM=true swift build --package-path /home/katei/ghq/github.com/swiftwasm/JavaScriptKit/Examples/Embedded -c release --triple wasm32-unknown-none-wasm 2>&1 | tee /tmp/repro.log
+
+if grep "swift-frontend: /home/build-user/llvm-project/llvm/lib/IR/Instructions.cpp:" /tmp/repro.log; then
+    echo "Reproducible"
+    exit 1
+else
+    echo "Not reproducible"
+    exit 0
+fi
+
