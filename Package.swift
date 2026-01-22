2e1202770432b3bc16330236452bec5153c7fb35
Max Desiatov <m_desiatov@apple.com>
2026-01-22 12:05:54 +0000

    Lower swift-syntax dependency range to 601

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
         .plugin(name: "BridgeJSCommandPlugin", targets: ["BridgeJSCommandPlugin"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax", "600.0.0"..<"603.0.0")
+        .package(url: "https://github.com/swiftlang/swift-syntax", "600.0.0"..<"601.0.0")
     ],
     targets: [
         .target(
