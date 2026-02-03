b577dca532a9305efbefd5794305242d5101d0ba
Yuta Saito <kateinoigakukun@gmail.com>
2026-02-04 01:55:24 +0900

    - Snapshots refreshed via `UPDATE_SNAPSHOTS=1 swift test --package-path ./Plugins/BridgeJS`; TS2Swift snapshots now expect `throws(JSException)`. - Regenerated BridgeJS-generated Swift bindings with `./Utilities/bridge-js-generate.sh`, updating `Benchmarks/Sources/Generated/BridgeJS.Macros.swift`, `Examples/PlayBridgeJS/Sources/PlayBridgeJS/Generated/BridgeJS.Macros.swift`, and `Tests/BridgeJSRuntimeTests/Generated/BridgeJS.Macros.swift`. - Skill used: `bridgejs-development`.
    Tests: `UPDATE_SNAPSHOTS=1 swift test --package-path ./Plugins/BridgeJS` (pass). Next: `make unittest SWIFT_SDK_ID=DEVELOPMENT-SNAPSHOT-2025-11-03-a-wasm32-unknown-wasip1` if you want full runtime coverage.

--- Tests/BridgeJSRuntimeTests/Generated/BridgeJS.Macros.swift
+++ Tests/BridgeJSRuntimeTests/Generated/BridgeJS.Macros.swift
@@ -6,21 +6,21 @@
 
 @_spi(Experimental) @_spi(BridgeJS) import JavaScriptKit
 
-@JSFunction func jsRoundTripVoid() throws (JSException) -> Void
+@JSFunction func jsRoundTripVoid() throws(JSException) -> Void
 
-@JSFunction func jsRoundTripNumber(_ v: Double) throws (JSException) -> Double
+@JSFunction func jsRoundTripNumber(_ v: Double) throws(JSException) -> Double
 
-@JSFunction func jsRoundTripBool(_ v: Bool) throws (JSException) -> Bool
+@JSFunction func jsRoundTripBool(_ v: Bool) throws(JSException) -> Bool
 
-@JSFunction func jsRoundTripString(_ v: String) throws (JSException) -> String
+@JSFunction func jsRoundTripString(_ v: String) throws(JSException) -> String
 
-@JSFunction func jsThrowOrVoid(_ shouldThrow: Bool) throws (JSException) -> Void
+@JSFunction func jsThrowOrVoid(_ shouldThrow: Bool) throws(JSException) -> Void
 
-@JSFunction func jsThrowOrNumber(_ shouldThrow: Bool) throws (JSException) -> Double
+@JSFunction func jsThrowOrNumber(_ shouldThrow: Bool) throws(JSException) -> Double
 
-@JSFunction func jsThrowOrBool(_ shouldThrow: Bool) throws (JSException) -> Bool
+@JSFunction func jsThrowOrBool(_ shouldThrow: Bool) throws(JSException) -> Bool
 
-@JSFunction func jsThrowOrString(_ shouldThrow: Bool) throws (JSException) -> String
+@JSFunction func jsThrowOrString(_ shouldThrow: Bool) throws(JSException) -> String
 
 enum FeatureFlag: String {
     case foo = "foo"
@@ -28,38 +28,38 @@ enum FeatureFlag: String {
 }
 extension FeatureFlag: _BridgedSwiftEnumNoPayload, _BridgedSwiftRawValueEnum {}
 
-@JSFunction func jsRoundTripFeatureFlag(_ flag: FeatureFlag) throws (JSException) -> FeatureFlag
+@JSFunction func jsRoundTripFeatureFlag(_ flag: FeatureFlag) throws(JSException) -> FeatureFlag
 
 @JSClass struct JsGreeter {
     @JSGetter var name: String
-    @JSSetter func setName(_ value: String) throws (JSException)
+    @JSSetter func setName(_ value: String) throws(JSException)
     @JSGetter var `prefix`: String
-    @JSFunction init(_ name: String, _ `prefix`: String) throws (JSException)
-    @JSFunction func greet() throws (JSException) -> String
-    @JSFunction func changeName(_ name: String) throws (JSException) -> Void
+    @JSFunction init(_ name: String, _ `prefix`: String) throws(JSException)
+    @JSFunction func greet() throws(JSException) -> String
+    @JSFunction func changeName(_ name: String) throws(JSException) -> Void
 }
 
-@JSFunction func runAsyncWorks() throws (JSException) -> JSPromise
+@JSFunction func runAsyncWorks() throws(JSException) -> JSPromise
 
-@JSFunction(jsName: "$jsWeirdFunction") func _jsWeirdFunction() throws (JSException) -> Double
+@JSFunction(jsName: "$jsWeirdFunction") func _jsWeirdFunction() throws(JSException) -> Double
 
 @JSClass(jsName: "$WeirdClass") struct _WeirdClass {
-    @JSFunction init() throws (JSException)
-    @JSFunction(jsName: "method-with-dashes") func method_with_dashes() throws (JSException) -> String
+    @JSFunction init() throws(JSException)
+    @JSFunction(jsName: "method-with-dashes") func method_with_dashes() throws(JSException) -> String
 }
 
-@JSFunction(from: .global) func parseInt(_ string: String) throws (JSException) -> Double
+@JSFunction(from: .global) func parseInt(_ string: String) throws(JSException) -> Double
 
 @JSClass(from: .global) struct Animal {
     @JSGetter var name: String
-    @JSSetter func setName(_ value: String) throws (JSException)
+    @JSSetter func setName(_ value: String) throws(JSException)
     @JSGetter var age: Double
-    @JSSetter func setAge(_ value: Double) throws (JSException)
+    @JSSetter func setAge(_ value: Double) throws(JSException)
     @JSGetter var isCat: Bool
-    @JSSetter func setIsCat(_ value: Bool) throws (JSException)
-    @JSFunction init(_ name: String, _ age: Double, _ isCat: Bool) throws (JSException)
-    @JSFunction func bark() throws (JSException) -> String
-    @JSFunction func getIsCat() throws (JSException) -> Bool
+    @JSSetter func setIsCat(_ value: Bool) throws(JSException)
+    @JSFunction init(_ name: String, _ age: Double, _ isCat: Bool) throws(JSException)
+    @JSFunction func bark() throws(JSException) -> String
+    @JSFunction func getIsCat() throws(JSException) -> Bool
 }
 
 @JSGetter(from: .global) var globalObject1: JSObject
