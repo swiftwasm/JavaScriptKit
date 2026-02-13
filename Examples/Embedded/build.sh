d0c02822ab7e9750273c6e7ae2255cbc3fcfa936
Yuta Saito <yuta@goodnotes.com>
2026-02-13 20:23:59 +0900

    CI: Use debug builds for SwiftSyntax in ./Examples/Embedded (#632)

--- Examples/Embedded/build.sh
+++ Examples/Embedded/build.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 package_dir="$(cd "$(dirname "$0")" && pwd)"
 swift package --package-path "$package_dir" \
-  -c release --swift-sdk "${SWIFT_SDK_ID_wasm32_unknown_wasip1:-${SWIFT_SDK_ID:-wasm32-unknown-wasip1}}-embedded" js
+  --swift-sdk "${SWIFT_SDK_ID_wasm32_unknown_wasip1:-${SWIFT_SDK_ID:-wasm32-unknown-wasip1}}-embedded" js -c release
