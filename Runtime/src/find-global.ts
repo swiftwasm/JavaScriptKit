a4206f2828e610954c40486a9e491d33edfc77d5
Krzysztof Rodak <krodak.konta@gmail.com>
2026-02-17 12:33:39 +0100

    Runtime: Simplify find-global.ts to just globalThis

--- Runtime/src/find-global.ts
+++ Runtime/src/find-global.ts
@@ -1,13 +1,1 @@
-interface GlobalVariable {}
-declare const global: GlobalVariable;
-
-export let globalVariable: any;
-if (typeof globalThis !== "undefined") {
-    globalVariable = globalThis;
-} else if (typeof window !== "undefined") {
-    globalVariable = window;
-} else if (typeof global !== "undefined") {
-    globalVariable = global;
-} else if (typeof self !== "undefined") {
-    globalVariable = self;
-}
+export const globalVariable: any = globalThis;
