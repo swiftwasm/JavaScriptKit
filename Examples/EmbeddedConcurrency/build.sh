ef9ab92891b5d0da1179fd7617b4014777281d37
Max Desiatov <m_desiatov@apple.com>
2026-03-16 23:32:58 +0000

    EmbeddedConcurrency: don't use `-c release` for SwiftSyntax
    Remove the '-c release' option from the build command.

--- Examples/EmbeddedConcurrency/build.sh
+++ Examples/EmbeddedConcurrency/build.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 package_dir="$(cd "$(dirname "$0")" && pwd)"
 swift package --package-path "$package_dir" \
   --swift-sdk "${SWIFT_SDK_ID_wasm32_unknown_wasip1:-${SWIFT_SDK_ID:-wasm32-unknown-wasip1}}-embedded" \
-  js --default-platform node -c release
+  js --default-platform node
 node "$package_dir/run.mjs"
