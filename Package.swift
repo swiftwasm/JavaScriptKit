0d6544f95427d6c1689b4908f961c0d5f566d5aa
Stephan Diederich <stephan.diederich@gmail.com>
2026-04-09 14:46:49 +0200

    relax swift-syntax version constraint to allow 603 (#714)
    * relax swift-syntax version constraint to allow 603
    
    Packages are updating to 603 and this can fail the dependency solver.
    
    * .. and also test against it

--- Package.swift
+++ Package.swift
@@ -48,7 +48,7 @@ let package = Package(
     ],
     traits: [tracingTrait],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax", "600.0.0"..<"603.0.0")
+        .package(url: "https://github.com/swiftlang/swift-syntax", "600.0.0"..<"604.0.0")
     ],
     targets: [
         .target(
